958f9de3c342ed0ff600a9a38c9126cb53175f25
Tarun <narwariyatarun05@gmail.com>
2022-12-07 22:12:15 +0530

    Worked on pages

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FAQsComponent } from './faqs/faqs.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutUsComponent,
+    FAQsComponent,
+    ContactUsComponent,
+    ServicesComponent
   ],
   imports: [
     CommonModule,
