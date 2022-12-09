55e146ae5a0e5df43b5dbc023600cbee2855713e
Tarun <narwariyatarun05@gmail.com>
2022-12-09 21:51:26 +0530

    Worked in UI

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -1,32 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { FAQsComponent } from './faqs/faqs.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
   },
-  {
-    path: 'faqs',
-    component: FAQsComponent
-  },
-  {
-    path: 'about-us',
-    component: AboutUsComponent
-  },
-  {
-    path: 'contact-us',
-    component: ContactUsComponent
-  },
-  {
-    path: 'services',
-    component: ServicesComponent
-  }];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,19 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { FAQsComponent } from './faqs/faqs.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutUsComponent,
-    FAQsComponent,
-    ContactUsComponent,
-    ServicesComponent
   ],
   imports: [
     CommonModule,
