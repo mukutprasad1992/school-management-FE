d3773da8aafec86547440a4d7ebeb79bf8bda445
Mukut Prasad <mukutprasad1992@gmail.com>
2022-12-23 17:25:08 +0530

    Worked on modularized the dashboard

---
+++ src/app/dashboard/common/charts/charts.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
+
+@Component({
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.scss'],
+})
+export class ChartsComponent {
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [
+    ['Download', 'Sales'],
+    ['In', 'Store', 'Sales'],
+    'Mail Sales',
+  ];
+  public pieChartDatasets = [
+    {
+      data: [300, 500, 100],
+    },
+  ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
+    responsive: false,
+  };
+  public radarChartLabels: string[] = [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running',
+  ];
+
+  public radarChartDatasets: ChartConfiguration<'radar'>['data']['datasets'] = [
+    { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' },
+  ];
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,9 +7,15 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { NgChartsModule } from 'ng2-charts';
 import { CalendarComponent } from './calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { ChartsComponent } from './common/charts/charts.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
 
 @NgModule({
-  declarations: [LandingComponent, AccountSettingsComponent, CalendarComponent],
+  declarations: [LandingComponent, AccountSettingsComponent, CalendarComponent, HeaderComponent, FooterComponent, SidebarComponent, MainComponent, ChartsComponent, TeachersComponent],
   imports: [
     CommonModule,
     FullCalendarModule,
